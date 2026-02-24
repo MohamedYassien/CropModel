fd661fbb31f82aba3a2503d2f7326c44300c5555
youssef-ibrahim23 <youssefabrahem6@gmail.com>
2026-02-24 13:39:39 +0200

    safe area handle

--- lib/features/sign_up/presentation/UI/sign_up_presenter.dart
+++ lib/features/sign_up/presentation/UI/sign_up_presenter.dart
@@ -44,123 +44,125 @@ class _SignUpPresenterState extends State<SignUpPresenter> {
         },
         child: BlocBuilder<SignUpBloc, SignUpState>(
           builder: (context, state) {
-            return Scaffold(
-              body: Center(
-                child: SingleChildScrollView(
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(height: 80.h),
-                        Image.asset('assets/images/logo.png'),
-                        SizedBox(height: 30.h),
-                        Text(
-                          'sign_up'.tr(),
-                          style: TextStyle(
-                            fontSize: 35.sp,
-                            fontWeight: FontWeight.bold,
+            return SafeArea(
+              child: Scaffold(
+                body: Center(
+                  child: SingleChildScrollView(
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(height: 80.h),
+                          Image.asset('assets/images/logo.png'),
+                          SizedBox(height: 30.h),
+                          Text(
+                            'sign_up'.tr(),
+                            style: TextStyle(
+                              fontSize: 35.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 40.h),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 35.r),
-                          child: CustomTextField(
-                            hintText: 'full_name'.tr(),
-                            controller: _fullNameController,
-                            validator: (value) =>
-                                Helpers.validateFullName(value),
+                          SizedBox(height: 40.h),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 35.r),
+                            child: CustomTextField(
+                              hintText: 'full_name'.tr(),
+                              controller: _fullNameController,
+                              validator: (value) =>
+                                  Helpers.validateFullName(value),
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 20.h),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 35.r),
-                          child: CustomTextField(
-                            hintText: 'email_address'.tr(),
-                            controller: _emailController,
-                            validator: (value) => Helpers.validateEmail(value),
+                          SizedBox(height: 20.h),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 35.r),
+                            child: CustomTextField(
+                              hintText: 'email_address'.tr(),
+                              controller: _emailController,
+                              validator: (value) => Helpers.validateEmail(value),
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 20.h),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 35.r),
-                          child: CustomTextField(
-                            hintText: 'phone_number'.tr(),
-                            controller: _phoneController,
-                            keyboardType: TextInputType.phone,
-                            validator: (value) => Helpers.validatePhone(value),
+                          SizedBox(height: 20.h),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 35.r),
+                            child: CustomTextField(
+                              hintText: 'phone_number'.tr(),
+                              controller: _phoneController,
+                              keyboardType: TextInputType.phone,
+                              validator: (value) => Helpers.validatePhone(value),
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 70.h),
-                        state is SignUpLoading
-                            ? CircularProgressIndicator(
-                                color: AppColors.primaryColor,
-                              )
-                            : Container(
-                                width: 284.w,
-                                height: 52.h,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15),
+                          SizedBox(height: 70.h),
+                          state is SignUpLoading
+                              ? CircularProgressIndicator(
                                   color: AppColors.primaryColor,
-                                ),
-                                child: MaterialButton(
-                                  child: Text(
-                                    'continue'.tr(),
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16.sp,
-                                      fontFamily: 'Nunito',
-                                      fontWeight: FontWeight.bold,
-                                    ),
+                                )
+                              : Container(
+                                  width: 284.w,
+                                  height: 52.h,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15),
+                                    color: AppColors.primaryColor,
                                   ),
-                                  onPressed: () {
-                                    FocusScope.of(context).unfocus();
-
-                                    if (_formKey.currentState!.validate()) {
-                                      context.read<SignUpBloc>().add(
-                                        SignUpButtonPressed(
-                                          signUpRequest: SignUpRequest(
-                                            name: _fullNameController.text,
-                                            email: _emailController.text,
-                                            phone: _phoneController.text,
+                                  child: MaterialButton(
+                                    child: Text(
+                                      'continue'.tr(),
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16.sp,
+                                        fontFamily: 'Nunito',
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      FocusScope.of(context).unfocus();
+              
+                                      if (_formKey.currentState!.validate()) {
+                                        context.read<SignUpBloc>().add(
+                                          SignUpButtonPressed(
+                                            signUpRequest: SignUpRequest(
+                                              name: _fullNameController.text,
+                                              email: _emailController.text,
+                                              phone: _phoneController.text,
+                                            ),
                                           ),
-                                        ),
-                                      );
-                                    }
-                                  },
+                                        );
+                                      }
+                                    },
+                                  ),
                                 ),
-                              ),
-                        SizedBox(height: 100.h),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "have_an_account_already".tr(),
-                              style: TextStyle(
-                                color: Color(0xff62707D),
-                                fontSize: 16.sp,
-                              ),
-                            ),
-                            TextButton(
-                              style: TextButton.styleFrom(
-                                padding: EdgeInsets.zero,
-                              ),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              child: Text(
-                                "login".tr(),
+                          SizedBox(height: 100.h),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "have_an_account_already".tr(),
                                 style: TextStyle(
-                                  color: AppColors.primaryColor,
-                                  fontFamily: 'Nunito',
+                                  color: Color(0xff62707D),
                                   fontSize: 16.sp,
-                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
+                              TextButton(
+                                style: TextButton.styleFrom(
+                                  padding: EdgeInsets.zero,
+                                ),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text(
+                                  "login".tr(),
+                                  style: TextStyle(
+                                    color: AppColors.primaryColor,
+                                    fontFamily: 'Nunito',
+                                    fontSize: 16.sp,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
