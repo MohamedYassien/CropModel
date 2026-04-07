431490c4a0f7a740ad1078a2578cca1f1748b336
mariomaayossrey <mariomayossrey@gmail.com>
2026-04-07 12:04:17 +0200

    Done Splach handle SnackBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'package:cropmodel/features/Login/presentation/UI/loginpage.dart';
-import 'package:cropmodel/features/Profile/presentation/UI/profile_presenter.dart';
+import 'package:cropmodel/features/Login/presentation/UI/Splach.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'features/Reset_password/presentaion/UI/confirm_reset_password.dart';
-import 'features/Reset_password/presentaion/UI/reseetpassword_present.dart';
 import 'features/Reset_password/presentaion/bloc/reset_password_bloc.dart';
 
 void main() async {
@@ -51,9 +48,10 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           builder: EasyLoading.init(),
           locale: context.locale,
+
           supportedLocales: context.supportedLocales,
           localizationsDelegates: context.localizationDelegates,
-          home: const ResetPasswordScreen(),
+          home:  Splach(),
         );
       },
     );
